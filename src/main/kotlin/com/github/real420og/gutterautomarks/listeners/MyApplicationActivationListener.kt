87132d9bc0608735bdab13c09a1ef1fe93eeae67
GitHub Action <action@github.com>
2024-01-13 09:40:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/real420og/gutterautomarks/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/real420og/gutterautomarks/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.real420og.gutterautomarks.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/real420og/gutterautomarks/services/MyProjectService.kt
+++ src/main/kotlin/com/github/real420og/gutterautomarks/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.real420og.gutterautomarks.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.real420og.gutterautomarks.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
